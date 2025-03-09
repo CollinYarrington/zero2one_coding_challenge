20c4158c0377b49353edb1bad49d38414c1215be
Collin Yarrington <collinyarrington.za@gmail.com>
2025-03-09 22:37:57 +0200

    ✨ Added seeder to generate two test users

---
+++ database/seeders/CreateTestUsersSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CreateTestUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => "John Doe",
+            'email' => "john@test.com",
+            'password' => Hash::make("1"),
+        ]);
+
+        User::create([
+            'name' => "Jane Doe",
+            'email' => "jane@test.com",
+            'password' => Hash::make("1"),
+        ]);
+    }
+}
