ea13802fe7381d94805b50c8d0a7332c28e5e04c
Collin Yarrington <collinyarrington.za@gmail.com>
2025-03-09 11:43:46 +0200

    ✨ Added login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
+use Inertia\Inertia;
+
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('login.attempt');
+});
+
+Route::group(['prefix' => 'login', 'as' => 'login.'], function () {
+    Route::get('/', [AuthenticationController::class, 'create']);
+    Route::post('/', [AuthenticationController::class, 'store'])->name('attempt');
 });
