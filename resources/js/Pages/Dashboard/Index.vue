3a1de7b2a81b3a3ff4db374221d9ead83fb29f3d
Collin Yarrington <collinyarrington.za@gmail.com>
2025-03-09 22:50:13 +0200

    ✨ Added dashboard that contains both the search and watchlist components

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Search from '../../Components/Search.vue';
+import WatchList from '../../Components/Watchlist.vue';
+import { ref } from 'vue';
+
+const newWatchListItem = ref();
+
+
+</script>
+<template>
+    <AppLayout>
+        <WatchList :newWatchListItem="newWatchListItem" />
+        <Search @updateWatchList="(item) => newWatchListItem = item"/>
+    </AppLayout>
+</template>
