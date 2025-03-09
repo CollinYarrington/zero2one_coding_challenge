b089bddb443862a16709ef60fc1d39a4e63d49f0
Collin Yarrington <collinyarrington.za@gmail.com>
2025-03-09 11:46:12 +0200

    ✨ Added essential input related components

---
+++ resources/js/Components/Ui/Input/InputText.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const props = defineProps({
+    modelValue: String | Number,
+    class: String,
+    error: Boolean,
+});
+
+const emits = defineEmits(['update:modelValue']);
+
+const modelValue = defineModel();
+</script>
+
+<template>
+    <input
+        v-model="modelValue"
+        :class="[
+            'p-2 border-2 rounded-lg focus-visible:ring-blue-400  focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1',
+            {
+                'border-red-500': error
+            }
+        ]"
+    />
+</template>
+
---
+++ resources/js/Components/Ui/Label/Label.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    label: String,
+    required: Boolean
+});
+
+</script>
+
+<template>
+    <label
+        :class="[
+            'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+            ]"
+    >
+    <slot /> <span v-if="required" class="text-red-500"> * </span>
+    </label>
+</template>
