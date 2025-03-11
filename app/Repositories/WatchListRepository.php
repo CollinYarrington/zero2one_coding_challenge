24ae18aac51603f28a39e3dc2cb293dfb521d8b1
Collin Yarrington <collinyarrington.za@gmail.com>
2025-03-11 06:38:06 +0200

    ✨ Added the ability to get the watchlist + add movies to the watchlist + delete movies from the watchlist, in the WatchlistRepository.php

--- app/Repositories/WatchListRepository.php
+++ app/Repositories/WatchListRepository.php
@@ -2,17 +2,34 @@
 
 namespace App\Repositories;
 
-use App\Models\WatchList;
+use App\Models\User;
+use App\Models\Watchlist;
 
-class WatchListRepository extends BaseRepository
+class WatchlistRepository extends BaseRepository
 {
-    public function __construct(WatchList $watchList)
+    public function __construct(Watchlist $watchlist)
     {
-        parent::__construct($watchList);
+        parent::__construct($watchlist);
     }
 
-    public function addToWatchList($data)
+    public function addToWatchlist($data)
     {
         return $this->model->create($data);
+    }
+
+    public function getPaginatedWatchlist(User $user, $items_per_page = 5, $page)
+    {
+        return $user->watchlist()->orderBy('created_at', 'desc')->paginate($items_per_page, ['*'], 'page', $page);
+    }
+
+    public function deleteFromWatchlist(User $user, $imdb_id, $items_per_page = 5, $page)
+    {
+        $user->watchlist()->where('imdb_id', $imdb_id)->delete();
+        return $this->getPaginatedWatchlist($user, $items_per_page, $page);
+    }
+
+    public function getWatchlist(User $user)
+    {
+        return $user->watchlist();
     }
 }
